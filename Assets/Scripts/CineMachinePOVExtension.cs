53ae3aad023f58d482bb6b2af925ff82ffe682f1
Fab <fabiomiguelchenhuang@gmail.com>
2023-11-13 14:31:59 +0000

    trying to implement right hand

--- Assets/Scripts/CineMachinePOVExtension.cs
+++ Assets/Scripts/CineMachinePOVExtension.cs
@@ -2,23 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
+using Leap.Unity;
+using Leap;
 
 public class CineMachinePOVExtension : CinemachineExtension
 {
     [SerializeField]
     private float clampAngle = 80f;
     [SerializeField]
-    private float verticalSpeed = 40f; // Look around speed in the vertical axis
+    private float verticalSpeed = 1f; // Look around speed in the vertical axis
     [SerializeField]
-    private float horizontalSpeed = 40f; // Look around speed in the horizontal axis
+    private float horizontalSpeed = 1f; // Look around speed in the horizontal axis
 
 
     private InputManager inputManager;
     private Vector3 startingRotation;
+    private Vector3 lastTipPosition;
+    private Vector2 deltaTipPosition;
+    GameObject playerObject;
+
     protected override void Awake()
     {
+        GameObject[] playerObject = GameObject.FindGameObjectsWithTag("Player");
         inputManager = InputManager.Instance;
         base.Awake();
+    }
+
+    private void Update()
+    {
+        Hand rightHand = Hands.Provider.GetHand(Chirality.Right);
+        //List<Hand> _allHands = Hands.Provider.CurrentFrame.Hands;
+        if (rightHand == null)
+        {
+            deltaTipPosition = new Vector2(0, 0);
+            return;
+        }
+
+        Vector3 rightHandVector = rightHand.Basis.translation.Pivot(Camera.main.transform.position, Quaternion.Inverse(Camera.main.transform.rotation)) - playerObject.transform.position;
+
+        if (lastTipPosition == null)
+        {
+            lastTipPosition = rightHandVector;
+        }
+
+        //Finger _middle = rightHand.GetMiddle();
+
+        Vector3 positionChange = lastTipPosition - rightHandVector;
+
+        lastTipPosition = rightHandVector;
+
+        deltaTipPosition = new Vector2(positionChange.x, positionChange.y);
+
+        //print(deltaTipPosition);
+
+
     }
 
     protected override void PostPipelineStageCallback(CinemachineVirtualCameraBase vcam, CinemachineCore.Stage stage, ref CameraState state, float deltaTime)
@@ -30,11 +67,19 @@ public class CineMachinePOVExtension : CinemachineExtension
                 if (startingRotation == null)
                 {
                     startingRotation = transform.localRotation.eulerAngles;
-                }
 
-                Vector2 deltaInput = inputManager.GetMouseDelta();
-                startingRotation.x += deltaTime * verticalSpeed *deltaInput.x;
-                startingRotation.y += deltaTime * horizontalSpeed * deltaInput.y;
+
+                }
+                //Hand rightHand = Hands.Provider.GetHand(Chirality.Right);
+                //List<Hand> _allHands = Hands.Provider.CurrentFrame.Hands;
+
+                //Finger _middle = rightHand.GetMiddle();
+
+                //print(_middle.TipPosition);
+
+                //Vector2 deltaInput = inputManager.GetMouseDelta();
+                startingRotation.x += deltaTime * verticalSpeed * deltaTipPosition.x;
+                startingRotation.y += deltaTime * horizontalSpeed * deltaTipPosition.y;
 
                 startingRotation.y = Mathf.Clamp(startingRotation.y, -clampAngle, clampAngle);
 
