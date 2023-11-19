981d710497dcded72ca9f556bb1500cad00995e1
luismrguimaraes <guimaraesluis0@gmail.com>
2023-11-19 12:46:57 +0000

    feat: add clipping to camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,7 +7,6 @@ public class CameraController : MonoBehaviour
 {
     // Camera
     public float cameraSpeed = 12.0f;
-    public float moveSpeed = 2.0f;
     public float minYAngle = -90.0f;
     public float maxYAngle = 90.0f;
     private float rotX;
@@ -32,18 +31,20 @@ public class CameraController : MonoBehaviour
 
         rotY += mouseInfo.x * cameraSpeed * Time.deltaTime;
 
-        print(inOrbit);
+        float newYAngle = transform.eulerAngles.x - mouseInfo.y * cameraSpeed * Time.deltaTime;
+        print(newYAngle);
+
         if (inOrbit) {
             orbitCentre.transform.RotateAround(orbitCentre.transform.position, Vector3.up, mouseInfo.x * cameraSpeed * Time.deltaTime);
             transform.RotateAround(orbitCentre.transform.position, Vector3.up, mouseInfo.x * cameraSpeed * Time.deltaTime);
             
-            float newYAngle = transform.eulerAngles.x - mouseInfo.y * cameraSpeed * Time.deltaTime;
-            //print(newYAngle);
-            //if (newYAngle < maxYAngle && newYAngle > minYAngle)
-            transform.RotateAround(orbitCentre.transform.position, orbitCentre.transform.right, -mouseInfo.y * cameraSpeed * Time.deltaTime);
+            if (newYAngle < maxYAngle || newYAngle > 360f + minYAngle)
+                transform.RotateAround(orbitCentre.transform.position, orbitCentre.transform.right, -mouseInfo.y * cameraSpeed * Time.deltaTime);
             
         }else {
             transform.RotateAround(transform.position, Vector3.up, mouseInfo.x * cameraSpeed * Time.deltaTime);
+
+            if (newYAngle < maxYAngle || newYAngle > 360f + minYAngle)
             transform.RotateAround(transform.position, transform.right, -mouseInfo.y * cameraSpeed * Time.deltaTime);
         }
     }
