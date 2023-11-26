7bdb5fb57f6979bcace3464a219b6bf5ce23dc66
Fab <fabiomiguelchenhuang@gmail.com>
2023-11-26 15:42:21 +0000

    feat: added arrow asset

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -33,7 +33,7 @@ public class PlayerController : MonoBehaviour
     void Update ()
     {
         Vector3 movement = new Vector3(leftHand.x_value * leftHandScaleFactorX, leftHand.y_value * leftHandScaleFactorY, leftHand.z_value * leftHandScaleFactorZ);
-        //Vector3 movement = inputManager.GetPlayerMovement();
+        movement = inputManager.GetPlayerMovement();
 
         Vector3 move;
         if (cam.inOrbit){
